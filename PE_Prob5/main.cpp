2dfcdd95f6b45cd5567f81dcfda3a5ba74c18b73
Chris Engstrom <tipher88@gmail.com>
2012-03-12 21:12:47 -0500

    Initial commit.

---
+++ PE_Prob5/main.cpp
@@ -0,0 +1,32 @@
+//****************************
+// Chris Engstrom            *
+// Project Euler - Problem 5 *
+//*******************************************************************
+// What is the smallest positive number that is evenly divisible by *
+//  all of the numbers from 1 to 20?                                *
+//*******************************************************************
+
+#include <iostream>
+
+using namespace std;
+
+// FUNCTION PROTOTYPES //
+bool checkPalindrome(int num);      // Checks a number to see if it is a palindrome
+
+int main() {
+        
+    
+
+    return 0;
+}
+
+//*******************************************************************************
+// FUNCTION: bool checkPalindrome(int num)                                      *
+//  int num - Number that will be checked to see if it is a palindrome          *
+//                                                                              *
+// RETURNS - Whether or not the number passed in is a palindrome                *
+//*******************************************************************************
+bool checkPalindrome(int num)
+{
+    
+}
